779c27d9a7357cfcc3d858162675c747fb9ab5b7
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-05-13 16:31:17 +0200

    define data structures inside messages

--- src/main/java/it/unitn/ds1/project/Messages.java
+++ src/main/java/it/unitn/ds1/project/Messages.java
@@ -1,15 +1,25 @@
 package it.unitn.ds1.project;
 
+import scala.Int;
+
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 public class Messages {
-
 
     public static class ClientUpdate implements Serializable {
         final int value;
 
         public ClientUpdate(int value) {
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "ClientUpdate{" +
+                    "value=" + value +
+                    '}';
         }
     }
 
@@ -19,41 +29,130 @@ public class Messages {
         public ReplicaUpdate(int value) {
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return "ReplicaUpdate{" +
+                    "value=" + value +
+                    '}';
+        }
     }
 
     public static class MasterUpdate implements Serializable {
         final int value;
 
-        public MasterUpdate(int value) {
+        final Timestamp timestamp;
+
+        public MasterUpdate(int value, Timestamp timestamp) {
             this.value = value;
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return "MasterUpdate{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+
+    public static class ReplicaUpdateAck implements Serializable {
+        final Timestamp timestamp;
+
+        public ReplicaUpdateAck(Timestamp timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return "ReplicaUpdateAck{" +
+                    "timestamp=" + timestamp +
+                    '}';
         }
     }
 
     public static class MasterUpdateOk implements Serializable {
+        final Timestamp timestamp;
 
+        public MasterUpdateOk(Timestamp timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return "MasterUpdateOk{" +
+                    "timestamp=" + timestamp +
+                    '}';
+        }
     }
 
-    public static class MasterHeartBeet implements Serializable {
-
+    public static class MasterHeartBeat implements Serializable {
+        @Override
+        public String toString() {
+            return "MasterHeartBeat";
+        }
     }
 
     public static class ReplicaElection implements Serializable {
+        final Map<Int, List<Timestamp>> historyByNodeId;
 
-    }
+        public ReplicaElection(Map<Int, List<Timestamp>> historyByNodeId) {
+            this.historyByNodeId = historyByNodeId;
+        }
 
-    public static class ReplicaAck implements Serializable {
 
+        @Override
+        public String toString() {
+            return "ReplicaElection{" +
+                    "historyByNodeId=" + historyByNodeId +
+                    '}';
+        }
     }
 
     public static class MasterSync implements Serializable {
+        final List<Timestamp> history;
+        final Int masterId;
 
+        public MasterSync(List<Timestamp> history, Int masterId) {
+            this.history = history;
+            this.masterId = masterId;
+        }
+
+        @Override
+        public String toString() {
+            return "MasterSync{" +
+                    "history=" + history +
+                    ", masterId=" + masterId +
+                    '}';
+        }
     }
 
-    public static class ReplicaVote implements Serializable {
-
+    public static class ReplicaElectionAck implements Serializable {
+        @Override
+        public String toString() {
+            return "ReplicaElectionAck";
+        }
     }
 
     public static class ClientRead implements Serializable {
+        @Override
+        public String toString() {
+            return "ClientRead";
+        }
+    }
 
+    public static class ReplicaReadReply implements Serializable {
+        final int value;
+
+        public ReplicaReadReply(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "ReplicaReadReply{" +
+                    "value=" + value +
+                    '}';
+        }
     }
 }
---
+++ src/main/java/it/unitn/ds1/project/Timestamp.java
@@ -0,0 +1,52 @@
+package it.unitn.ds1.project;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+import static java.util.Comparator.comparingInt;
+
+public class Timestamp implements Comparable<Timestamp> {
+
+    /**
+     * referred as 'i' in the project document
+     */
+    final int counter;
+
+    /**
+     * referred as 'e' in the project document
+     */
+    final int epoch;
+
+    public Timestamp(int counter, int epoch) {
+        this.counter = counter;
+        this.epoch = epoch;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Timestamp timestamp = (Timestamp) o;
+        return counter == timestamp.counter &&
+                epoch == timestamp.epoch;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(counter, epoch);
+    }
+
+    @Override
+    public String toString() {
+        return "<" + epoch + ", " + counter + ">";
+    }
+
+    private static final Comparator<Timestamp> COMPARATOR =
+            comparingInt((Timestamp timestamp) -> timestamp.epoch)
+                    .thenComparingInt(timestamp -> timestamp.counter);
+
+    @Override
+    public int compareTo(Timestamp o) {
+        return COMPARATOR.compare(this, o);
+    }
+}
